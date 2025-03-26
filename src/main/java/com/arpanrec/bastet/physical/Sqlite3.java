462b8459de8cb38b6629d08776064adab2b01031
arpan <arpan.rec@gmail.com>
2025-03-26 22:28:20 +0530

    Refactor Sqlite3 class to set default path and improve table creation logic

--- src/main/java/com/arpanrec/bastet/physical/Sqlite3.java
+++ src/main/java/com/arpanrec/bastet/physical/Sqlite3.java
@@ -5,14 +5,18 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Map;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class Sqlite3 extends SqlBackend {
 
-    private final String path;
+    private String path = "bastet.sqlite3";
 
     public Sqlite3(Map<String, Object> config) {
-        this.path = config.get("path").toString();
-        super.createTable();
+        if (config.containsKey("path") && !config.get("path").toString().isEmpty()) {
+            this.path = config.get("path").toString();
+        }
+        createTable();
     }
 
     protected Connection getConnect() {
